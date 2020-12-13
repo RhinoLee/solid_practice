4d40a188eb43a17a16d6134d844dea6b8550aa61
RhinoLee <picklerhino@gmail.com>
2020-12-14 02:46:56 +0800

    function 3 final

--- practice.js
+++ practice.js
@@ -20,25 +20,49 @@ let point = 70
 // 調整 color 進 pointColor
 // 缺點： 調整 point 要進 function, （因為判斷式）
 
+// const pointColor = ['red', 'yellow', 'green']
+// function checkPoint1(point){
+//   switch (true){
+//     case point < 60 : 
+//       return 0 
+//     case point < 75 : 
+//       return 1 
+//     case point >= 75 : 
+//       return 2
+//     default: 
+//       return 
+//   }
+
+// }
+
+// function checkColor(){
+//   return pointColor[checkPoint1(point)]
+// }
+// document.style.background = pointColor[checkPoint1(point)]
+
+
+
+// 方法3: 
+// 之後修改資料即可
+const pointLevel = [60, 75]
 const pointColor = ['red', 'yellow', 'green']
-function checkPoint1(point){
-  switch (true){
-    case point < 60 : 
-      return 0 
-    case point < 75 : 
-      return 1 
-    case point >= 75 : 
-      return 2
-    default: 
-      return 
+
+function checkPoint2(point) {
+  let level = 0
+  for(let i = 0; i < pointLevel; i++){
+    if(point > pointLevel[i]) { level += 1 }
   }
 
+  return level 
 }
+
 
 function checkColor(){
-  return pointColor[checkPoint1(point)]
+  return pointColor[checkPoint2(point)]
 }
-document.style.background = pointColor[checkPoint1(point)]
 
+console.log(pointColor[checkPoint2(point)]); // 'red'
+
+// document.style.background = pointColor[checkPoint1(point)]
 
 
