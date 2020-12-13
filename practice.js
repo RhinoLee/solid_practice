295b9554cdf0a23b194c02ce8c77c4d349d7fe41
RhinoLee <picklerhino@gmail.com>
2020-12-14 01:22:32 +0800

    function 1

---
+++ practice.js
@@ -0,0 +1,15 @@
+// 使用情境
+// 根據 point 設定對應顏色
+
+// 方法1:
+function checkPoint(){
+  if(point < 60){
+    document.style.background = "red"
+  }else if(point < 75){
+    document.style.background = "yellow"
+  }else {
+    document.style.background = "green"
+  }
+}
+
+
